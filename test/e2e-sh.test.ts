49d5fb1d951444d6213ed07dc6079ee8765f6d90
Jacob Hummer <jcbhmr@outlook.com>
2023-05-08 18:15:01 +0000

    add test

---
+++ test/e2e-sh.test.ts
@@ -0,0 +1,40 @@
+import $ from "https://deno.land/x/dax/mod.ts";
+import { expect } from "https://deno.land/x/expect/mod.ts";
+
+Deno.test("deno-wrapper", async (t) => {
+  const tempFolder = await Deno.makeTempDir();
+  try {
+    Deno.chdir(tempFolder);
+
+    await t.step("deno run -A src/index.ts v1.30.0", async () => {
+      const indexTSPath = import.meta.resolve("../src/index.ts");
+      await $`deno run -A ${indexTSPath} v1.30.0`;
+    });
+
+    await t.step("file ./denow and ./denow.bat exist", async () => {
+      console.log("./denow");
+      console.log(await Deno.readTextFile("./denow"));
+
+      console.log("./denow.bat");
+      console.log(await Deno.readTextFile("./denow.bat"));
+    });
+
+    await t.step("./denow eval 'console.log(Deno.version.deno)'", async () => {
+      const output =
+        await $`./denow eval 'console.log(Deno.version.deno)'`.text();
+      expect(output).toEqual("1.30.0");
+    });
+
+    await t.step("folder .deno exists", async () => {
+      const items = [];
+      for await (const item of Deno.readDir(".deno")) {
+        items.push(item.name);
+      }
+
+      console.log(".deno");
+      console.log(items);
+    });
+  } finally {
+    await Deno.remove(tempFolder, { recursive: true });
+  }
+});
